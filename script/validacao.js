206df6b7a683b79d5f93a37230d40703dacd7771
matheus_guesser <matheus_guesser@estudante.sesisenai.org.br>
2026-05-06 12:54:30 -0300

    add- progresso no login

--- script/validacao.js
+++ script/validacao.js
@@ -1,19 +1,28 @@
-function login() {
-    let usuario = document.getElementById("usuario").value;
-    let senha = document.getElementById("senha").value;
-    let erro = document.getElementById("erro");
+document.getElementById("form-login").onsubmit = (e) => {
+    e.preventDefault();
 
-    let userCorreto = "admin";
-    let senhaCorreta = "1234";
+    let usuario = document.getElementById("usuario").value.trim();
+    let senha = document.getElementById("senha").value.trim();
+    let mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML = "";
 
-    if (usuario === "" || senha === "") {
-        erro.innerText = "Preencha todos os campos!";
-        return;
-    }
 
-    if (usuario !== userCorreto || senha !== senhaCorreta) {
-        erro.innerText = "Usuário ou senha incorretos!";
-        return;
-    }
+
+   let salva = localStorage.getItem(usuario);
+
+
+    console.log("usuário digitado:", usuario);
+    console.log("senha digitada:", senha);
+    console.log("senha salva:", salva);
+
     
-}
+    if (salva === senha) {
+        mensagem.innerHTML = "<div class='alert alert-success'>Login realizado com sucesso!</div>";
+        window.location.href = "public/tela-geral-home.html";
+    } else {
+        mensagem.innerHTML = "<div class='alert alert-danger'>Usuário ou senha incorretos!</div>";
+    }
+
+    document.getElementById("form-login").reset();
+};
+
